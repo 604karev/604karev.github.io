eafb7ac32b91a10ba6439a7dc0ec18c1c3ca2d55
604karev <604karev@gmail.com>
2019-05-22 13:40:07 +0300

    commit

---
+++ precache-manifest.196dfaf209d52e6448fc175c404a3537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a051ea0e1e410d92977a3533d5aadf7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d107eade7f0bd75b36b",
+    "url": "/static/css/2.b9c5add1.chunk.css"
+  },
+  {
+    "revision": "e5f12246dbdde115af28",
+    "url": "/static/css/main.c9b60a5b.chunk.css"
+  },
+  {
+    "revision": "9d107eade7f0bd75b36b",
+    "url": "/static/js/2.e5e1755a.chunk.js"
+  },
+  {
+    "revision": "e5f12246dbdde115af28",
+    "url": "/static/js/main.91aa288b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
